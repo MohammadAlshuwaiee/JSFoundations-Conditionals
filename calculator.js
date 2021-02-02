597293617a1013304d95db090c13afb8df0e35c4
mohammadalshuwaiee <shuwaiee1993@gmail.com>
2021-02-02 19:51:47 +0300

    Conditionals Task

--- calculator.js
+++ calculator.js
@@ -1,3 +1,13 @@
 // Define the numbers and the operation here
 
+FirstNumber = 15
+SecondNumber = 18
+Operation = "/"
+
 // Write if-statements here
+
+if (Operation == "/") {
+
+    console.log(FirstNumber / SecondNumber);
+}
+
